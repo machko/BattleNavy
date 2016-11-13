df66ef5f082793fc3ba5b291fdd6692429f6d5f3
machko <machko@users.noreply.github.com>
2016-11-13 21:55:26 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,37 +9,26 @@
 #       -Interface graphique                                                                                                    #
 #################################################################################################################################
 
-
 from random import randint
-
 plateau = []
-
 for x in range(0, 5):
     plateau.append(["O"] * 5)
-
 def afficher_plateau(plateau):
     for ligne in plateau:
         print " ".join(ligne)
-
 afficher_plateau(plateau)
-
 def alea_ligne(plateau):
     return randint(0, len(plateau) - 1)
-
 def alea_col(plateau):
     return randint(0, len(plateau[0]) - 1)
-
 bateau_x = alea_ligne(plateau)
 bateau_y = alea_col(plateau)
-
 #Pour le debug decommanté ligne ci-desous.
 #print bateau_x 
 #print bateau_y 
-
 for tour in range(4):
     tir_x = int(input("Quelle ligne ?"))
     tir_y = int(input("Quelle colonne ?"))
-
     if tir_x == bateau_x and tir_y == bateau_y:
         print "Bravo ! Vous avez eu mon bateau !"
         break
