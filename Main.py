cf3c1d354c4dfe6781059f47a9309c5222ac8e60
machko <machko@users.noreply.github.com>
2016-11-13 21:58:47 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ for x in range(0, 5):
     plateau.append(["O"] * 5)
 def afficher_plateau(plateau):
     for ligne in plateau:
-        print " ".join(ligne)
+        print(" ".join(ligne))
 afficher_plateau(plateau)
 def alea_ligne(plateau):
     return randint(0, len(plateau) - 1)
@@ -30,19 +30,19 @@ for tour in range(4):
     tir_x = int(input("Quelle ligne ?"))
     tir_y = int(input("Quelle colonne ?"))
     if tir_x == bateau_x and tir_y == bateau_y:
-        print "Bravo ! Vous avez eu mon bateau !"
+        print("Bravo ! Vous avez eu mon bateau !")
         break
     else:
         if tour >= 3:
             print "Game Over"
             break
         if (tir_x < 0 or tir_x > 4) or (tir_y < 0 or tir_y > 4):
-            print "Oups ! Vous devez viser l'ocean."
+            print("Oups ! Vous devez viser l'ocean.")
         elif(plateau[tir_x ][tir_y ] == "X"):
-            print "Cette case est deja decouverte."
+            print("Cette case est deja decouverte.")
         else:
-            print "Dommage !"
+            print("Dommage !")
             plateau[tir_x ][tir_y ] = "X"
-            print "Tour", tour+1
+            print("Tour", tour+1)
             tour += 1
             afficher_plateau(plateau)
